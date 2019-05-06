bae718c6c7564e6674501a2f9947fd7062c323f5
gurikin <gurikin@yandex.ru>
2019-05-06 07:59:33 +0300

    first using services complete

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 
 class ArticleController extends AbstractController
@@ -13,9 +15,10 @@ class ArticleController extends AbstractController
    * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
    * @param $slug
    */
-  public function toggleArticleHeart($slug)
+  public function toggleArticleHeart($slug, LoggerInterface $logger)
   {
     // TODO - actually heart/unheart the article!
+    $logger->info('Article is being hearted!');
     return $this->json(['hearts' => rand(5, 100)]);
   }
 
@@ -31,7 +34,7 @@ class ArticleController extends AbstractController
    * @param $slug
    * @return Response
    */
-  public function show($slug)
+  public function show($slug, Environment $twigEnvironment)
   {
     $comments = [
       'I ate a normal rock once. It did NOT taste like bacon!',
@@ -39,10 +42,16 @@ class ArticleController extends AbstractController
       'I like bacon too! Buy some from my site! bakinsomebacon.com',
     ];
 
-    return $this->render('article/show.html.twig', [
+    /*return $this->render('article/show.html.twig', [
+      'title' => ucwords(str_replace('-', ' ', $slug)),
+      'comments' => $comments,
+      'slug' => $slug
+    ]);*/
+    $html = $twigEnvironment->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
       'comments' => $comments,
       'slug' => $slug
     ]);
+    return new Response($html);
   }
 }
