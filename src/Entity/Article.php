b258f656d46ec9c1330c56a91941b057da00c57b
gurikin <gurikin@yandex.ru>
2019-07-07 20:27:49 +0300

    add tag entity complete

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -66,9 +66,15 @@ class Article
    */
   private $comments;
 
+  /**
+   * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="articles")
+   */
+  private $tags;
+
   public function __construct()
   {
       $this->comments = new ArrayCollection();
+      $this->tags = new ArrayCollection();
   }
 
   /**
@@ -251,6 +257,34 @@ class Article
           if ($comment->getArticle() === $this) {
               $comment->setArticle(null);
           }
+      }
+
+      return $this;
+  }
+
+  /**
+   * @return Collection|Tag[]
+   */
+  public function getTags(): Collection
+  {
+      return $this->tags;
+  }
+
+  public function addTag(Tag $tag): self
+  {
+      if (!$this->tags->contains($tag)) {
+          $this->tags[] = $tag;
+          $tag->addArticle($this);
+      }
+
+      return $this;
+  }
+
+  public function removeTag(Tag $tag): self
+  {
+      if ($this->tags->contains($tag)) {
+          $this->tags->removeElement($tag);
+          $tag->removeArticle($this);
       }
 
       return $this;
