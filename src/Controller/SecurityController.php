4277ab70a03f70f43e175f6836d0bb585828c036
gurik <gurikin@yandex.ru>
2019-07-22 21:15:10 +0300

    29 manual register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,35 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-  /**
-   * @Route("/login", name="app_login")
-   * @param AuthenticationUtils $authenticationUtils
-   * @return Response
-   */
-  public function login(AuthenticationUtils $authenticationUtils)
-  {
-    $error = $authenticationUtils->getLastAuthenticationError();
-    $lastUserName = $authenticationUtils->getLastUsername();
-    return $this->render('security/login.html.twig', [
-      'error' => $error,
-      'last_username' => $lastUserName
-    ]);
-  }
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUserName = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUserName
+        ]);
+    }
 
-  /**
-   * @Route("/logout", name="app_logout")
-   * @throws Exception
-   */
-  public function logout()
-  {
-    return $this->redirectToRoute('app_login');
-  }
+    /**
+     * @Route("/logout", name="app_logout")
+     * @throws Exception
+     */
+    public function logout()
+    {
+        return $this->redirectToRoute('app_login');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     * @param GuardAuthenticatorHandler $guardAuthenticatorHandler
+     * @param LoginFormAuthenticator $loginFormAuthenticator
+     * @return Response
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $userPasswordEncoder, GuardAuthenticatorHandler $guardAuthenticatorHandler, LoginFormAuthenticator $loginFormAuthenticator)
+    {
+        // TODO - use Symfony forms & validation
+        if ($request->isMethod('POST')) {
+            $user = new User();
+            $user->setEmail($request->request->get('email'));
+            $user->setFirstName('Mister N');
+            $user->setPassword($userPasswordEncoder->encodePassword(
+                $user, $request->request->get('password'))
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $guardAuthenticatorHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $loginFormAuthenticator,
+                'main'
+            );
+        }
+
+        return $this->render('security/register.html.twig');
+    }
 }
