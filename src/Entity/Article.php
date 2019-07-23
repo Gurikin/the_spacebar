cbb1fcd88a54c7ebbe525e5a7d5542b338294203
gurik <gurikin@yandex.ru>
2019-07-23 07:17:48 +0300

    30 author many-to-one relation to user

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -16,277 +16,296 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
  */
 class Article
 {
-  use TimestampableEntity;
-  /**
-   * @ORM\Id()
-   * @ORM\GeneratedValue()
-   * @ORM\Column(type="integer")
-   */
-  private $id;
+    use TimestampableEntity;
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-  /**
-   * @ORM\Column(type="string", length=255)
-   */
-  private $title;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
 
-  /**
-   * @Gedmo\Slug(fields={"title"})
-   * @ORM\Column(type="string", length=128, unique=true)
-   */
-  private $slug;
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(type="string", length=128, unique=true)
+     */
+    private $slug;
 
-  /**
-   * @ORM\Column(type="text", nullable=true)
-   */
-  private $content;
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
 
-  /**
-   * @ORM\Column(type="datetime", nullable=true)
-   */
-  private $publishedAt;
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedAt;
 
-  /**
-   * @ORM\Column(type="string", length=255)
-   */
-  private $author;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $heartCount = 0;
 
-  /**
-   * @ORM\Column(type="integer")
-   */
-  private $heartCount = 0;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageFilename;
 
-  /**
-   * @ORM\Column(type="string", length=255, nullable=true)
-   */
-  private $imageFilename;
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article", fetch="EXTRA_LAZY")
+     * @ORM\OrderBy({"createdAt" = "DESC"})
+     */
+    private $comments;
 
-  /**
-   * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article", fetch="EXTRA_LAZY")
-   * @ORM\OrderBy({"createdAt" = "DESC"})
-   */
-  private $comments;
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="articles")
+     */
+    private $tags;
 
-  /**
-   * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="articles")
-   */
-  private $tags;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
 
-  public function __construct()
-  {
-      $this->comments = new ArrayCollection();
-      $this->tags = new ArrayCollection();
-  }
+    /**
+     * Article constructor.
+     */
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+        $this->tags = new ArrayCollection();
+    }
 
-  /**
-   * @return int|null
-   */
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-  /**
-   * @return string|null
-   */
-  public function getTitle(): ?string
-  {
-    return $this->title;
-  }
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
-  /**
-   * @param string $title
-   * @return Article
-   */
-  public function setTitle(string $title): self
-  {
-    $this->title = $title;
+    /**
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string|null
-   */
-  public function getSlug(): ?string
-  {
-    return $this->slug;
-  }
+    /**
+     * @return string|null
+     */
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
 
-  /**
-   * @param string $slug
-   * @return Article
-   */
-  public function setSlug(string $slug): self
-  {
-    $this->slug = $slug;
+    /**
+     * @param string $slug
+     * @return Article
+     */
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string|null
-   */
-  public function getContent(): ?string
-  {
-    return $this->content;
-  }
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
 
-  /**
-   * @param string|null $content
-   * @return Article
-   */
-  public function setContent(?string $content): self
-  {
-    $this->content = $content;
+    /**
+     * @param string|null $content
+     * @return Article
+     */
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return DateTimeInterface|null
-   */
-  public function getPublishedAt(): ?DateTimeInterface
-  {
-    return $this->publishedAt;
-  }
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getPublishedAt(): ?DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
 
-  /**
-   * @param DateTimeInterface|null $publishedAt
-   * @return Article
-   */
-  public function setPublishedAt(?DateTimeInterface $publishedAt): self
-  {
-    $this->publishedAt = $publishedAt;
+    /**
+     * @param DateTimeInterface|null $publishedAt
+     * @return Article
+     */
+    public function setPublishedAt(?DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string|null
-   */
-  public function getAuthor(): ?string
-  {
-    return $this->author;
-  }
+    /**
+     * @return int|null
+     */
+    public function getHeartCount(): ?int
+    {
+        return $this->heartCount;
+    }
 
-  /**
-   * @param string $author
-   * @return Article
-   */
-  public function setAuthor(string $author): self
-  {
-    $this->author = $author;
+    /**
+     * @param int $heartCount
+     * @return Article
+     */
+    public function setHeartCount(int $heartCount): self
+    {
+        $this->heartCount = $heartCount;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return int|null
-   */
-  public function getHeartCount(): ?int
-  {
-    return $this->heartCount;
-  }
+    /**
+     * @return string|null
+     */
+    public function getImageFilename(): ?string
+    {
+        return $this->imageFilename;
+    }
 
-  /**
-   * @param int $heartCount
-   * @return Article
-   */
-  public function setHeartCount(int $heartCount): self
-  {
-    $this->heartCount = $heartCount;
+    /**
+     * @param string|null $imageFilename
+     * @return Article
+     */
+    public function setImageFilename(?string $imageFilename): self
+    {
+        $this->imageFilename = $imageFilename;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string|null
-   */
-  public function getImageFilename(): ?string
-  {
-    return $this->imageFilename;
-  }
+    /**
+     * @return Article
+     */
+    public function incrementHeartCount(): self
+    {
+        $heartCount = $this->getHeartCount() + 1;
+        $this->setHeartCount($heartCount);
+        return $this;
+    }
 
-  /**
-   * @param string|null $imageFilename
-   * @return Article
-   */
-  public function setImageFilename(?string $imageFilename): self
-  {
-    $this->imageFilename = $imageFilename;
+    /**
+     * @return Collection
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
 
-    return $this;
-  }
+    /**
+     * @return Collection
+     */
+    public function getNonDeletedComments(): Collection
+    {
+        $criteria = CommentRepository::createNonDeletedCriteria();
+        return $this->comments->matching($criteria);
+    }
 
-  /**
-   * @return Article
-   */
-  public function incrementHeartCount(): self
-  {
-    $heartCount = $this->getHeartCount() + 1;
-    $this->setHeartCount($heartCount);
-    return $this;
-  }
+    /**
+     * @param Comment $comment
+     * @return Article
+     */
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
 
-  public function getComments(): Collection
-  {
-      return $this->comments;
-  }
+        return $this;
+    }
 
-  public function getNonDeletedComments(): Collection
-  {
-    $criteria = CommentRepository::createNonDeletedCriteria();
-    return $this->comments->matching($criteria);
-  }
+    /**
+     * @param Comment $comment
+     * @return Article
+     */
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
 
-  public function addComment(Comment $comment): self
-  {
-      if (!$this->comments->contains($comment)) {
-          $this->comments[] = $comment;
-          $comment->setArticle($this);
-      }
+        return $this;
+    }
 
-      return $this;
-  }
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
 
-  public function removeComment(Comment $comment): self
-  {
-      if ($this->comments->contains($comment)) {
-          $this->comments->removeElement($comment);
-          // set the owning side to null (unless already changed)
-          if ($comment->getArticle() === $this) {
-              $comment->setArticle(null);
-          }
-      }
+    /**
+     * @param Tag $tag
+     * @return Article
+     */
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+            $tag->addArticle($this);
+        }
 
-      return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return Collection|Tag[]
-   */
-  public function getTags(): Collection
-  {
-      return $this->tags;
-  }
+    /**
+     * @param Tag $tag
+     * @return Article
+     */
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tags->contains($tag)) {
+            $this->tags->removeElement($tag);
+            $tag->removeArticle($this);
+        }
 
-  public function addTag(Tag $tag): self
-  {
-      if (!$this->tags->contains($tag)) {
-          $this->tags[] = $tag;
-          $tag->addArticle($this);
-      }
+        return $this;
+    }
 
-      return $this;
-  }
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
 
-  public function removeTag(Tag $tag): self
-  {
-      if ($this->tags->contains($tag)) {
-          $this->tags->removeElement($tag);
-          $tag->removeArticle($this);
-      }
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
-      return $this;
-  }
+        return $this;
+    }
 }
