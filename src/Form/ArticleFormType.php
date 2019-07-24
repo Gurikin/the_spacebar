ac9f05e75023163c9c316a04757dd786a53c732d
gurik <gurikin@yandex.ru>
2019-07-24 23:10:59 +0300

    6 dateTime type and data transforming

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -19,6 +19,9 @@ class ArticleFormType extends AbstractType
                 'help' => 'Choose something catchy!'
             ])
             ->add('content')
+            ->add('publishedAt', null, [
+                'widget' => 'single_text'
+            ])
         ;
     }
 
