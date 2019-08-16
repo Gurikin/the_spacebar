98f2a677328c0356b83e6b95c84d4a9160513a22
gurik <gurikin@yandex.ru>
2019-08-16 08:08:48 +0300

    24 Data Transformer

--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -4,13 +4,46 @@
 namespace App\Form;
 
 
+use App\Form\DataTransformer\EmailToUserTransformer;
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * Class UserSelectTextType
+ * @package App\Form
+ */
 class UserSelectTextType extends AbstractType
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * UserSelectTextType constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @return string|null
+     */
     public function getParent()
     {
         return TextType::class;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new EmailToUserTransformer($this->userRepository));
     }
 }
